62714c6eae5c1de1b87ea17b85deaecdcdfca2d6
Ahmad ashraf <ahmed@octopuslabs.com>
2023-05-09 14:15:45 +0500

    removing log

--- main.go
+++ main.go
@@ -121,10 +121,6 @@ func main() {
 		judge = true
 	}
 
-	cosmos := getCosmosClient()
-	cosmosAddress := getCosmosAddress(accountName, cosmos)
-
-	fmt.Println(cosmosAddress.String())
 	time.Sleep(1 * time.Minute)
 
 	wg.Add(1)

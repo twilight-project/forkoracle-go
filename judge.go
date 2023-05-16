25b1c13c977ea75ad1cbeec5a4a9e0f7aa7b8f78
Ahmad ashraf <ahmed@octopuslabs.com>
2023-05-16 13:18:25 +0500

    adding dummy 0

--- judge.go
+++ judge.go
@@ -355,14 +355,14 @@ func generate_signed_tx(address string, accountName string, sweeptx *wire.MsgTx)
 		preimage := querySweepAddressPreimage(address)
 
 		for i := 0; i < len(sweeptx.TxIn); i++ {
-			witness := make([][]byte, 0)
+			witness := wire.TxWitness{}
+			dummy := []byte{0}
+			witness = append(witness, dummy)
 			for j := 0; j < len(dataSig); j++ {
 				witness = append(witness, dataSig[j])
 			}
 			witness = append(witness, preimage)
 			witness = append(witness, script)
-			dummy := []byte{0}
-			witness = append(witness, dummy)
 			sweeptx.TxIn[i].Witness = witness
 		}
 

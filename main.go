9721778e835e16d4c7c35d4828482ef05a278988
Ahmad ashraf <ahmed@octopuslabs.com>
2023-05-17 12:31:10 +0500

    debugging

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func initialize() {
 	valAddr_, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
-		fmt.Println("Val Address : ", valAddr_)
+		fmt.Printf("Output: %s\n", valAddr_)
 		return
 	}
 

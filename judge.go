2da3a5bbf8cde53a998709eb59753d811f450f8f
Ahmad ashraf <ahmed@octopuslabs.com>
2023-10-11 04:49:06 +0500

    debug

--- judge.go
+++ judge.go
@@ -420,7 +420,13 @@ func addressProposer(accountName string) {
 
 		RoundId, _ := strconv.Atoi(reserveToBeUpdated.RoundId)
 
+		fmt.Println("+++++++++++++++++++++")
+		fmt.Println(RoundId)
+		fmt.Println(reserveIdForProposal)
+		fmt.Println("+++++++++++++++++++++")
+
 		proposed := checkIfAddressIsProposed(int64(RoundId+1), int64(reserveIdForProposal))
+		fmt.Println(proposed)
 		if proposed == true {
 			continue
 		}

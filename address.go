bc6afd065f112be628f389dda6e2409dc28f79e4
Ahmad ashraf <ahmed@octopuslabs.com>
2024-02-14 19:41:37 +0500

    adding check

--- address.go
+++ address.go
@@ -249,14 +249,9 @@ func processProposeAddress(accountName string) {
 				continue
 			}
 
-			judges := getRegisteredJudges()
-
 			addresses := querySweepAddressesByHeight(uint64(height+sweepInitateBlockHeight), false)
-			if len(addresses) <= 0 && len(judges.Judges) == 1 {
-				addresses = querySweepAddressesByHeight(uint64(height+sweepInitateBlockHeight), true)
-				if len(addresses) <= 0 {
-					continue
-				}
+			if len(addresses) <= 0 {
+				continue
 			}
 
 			fmt.Println("Sweep Address found, proposing address for reserve : {}, round : {}", reserveIdForProposal, RoundId+1)
